9a081b4ad118f5e3fb1d9ab0bade5155363d984e
Frans Ekman <fransedvinekman@gmail.com>
2024-12-23 13:30:39 +0100

    hook unit tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,10 +4,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom",
-    include: ["./src/**/*.test.ts"],
+    include: ["./src/**/*.test.{ts,tsx}"],
     coverage: {
-      include: ["src/**/*.ts"],
-      exclude: ["src/index.ts", "src/**/*.test.ts"],
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: ["src/index.{ts,tsx}", "src/**/*.test.{ts,tsx}"],
       reporter: ["text", "json", "html"],
     },
     alias: {
